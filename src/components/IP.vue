870928db42f8a71198a27d6960a0079ff4dd7182
jadezi <857827224@qq.com>
2020-11-26 23:08:03 +0800

    添加不同网段比较功能

--- src/components/IP.vue
+++ src/components/IP.vue
@@ -33,7 +33,7 @@ export default {
   data() {
     return {
       netWork: "",
-      subNet: "10.18.1.0/24,10.18.19.0/23",
+      subNet: "10.18.1.0/24,10.18.19.0/26,10.18.19.128/26",
       result: "",
     };
   },
@@ -46,8 +46,28 @@ export default {
       this._printSubNet(this._splitNetWork(net));
     },
     _printSubNet(subNetwork) {
+      let group = []
       for(let i=0;i<subNetwork.length;i++){
-        console.log(this._getBrocastAddr(subNetwork[i].net[3],subNetwork[i].mask))
+        group.push({tag:subNetwork[i].net[2]+"."+subNetwork[i].net[3],standard:subNetwork[i].net[2]+'.0',netAdd:subNetwork[i].net[3],broAdd:this._getBrocastAddr(subNetwork[i].net[3],subNetwork[i].mask)})
+      }
+      console.log(group);
+      this._dffNetWork(group)
+    },
+    _dffNetWork(netWorkGroup){
+      console.log('start');
+      console.log(netWorkGroup);
+      let resGroup = []
+      for(let i = 0;i<netWorkGroup.length;i++){
+        for(let j = 1;j<netWorkGroup.length;j++){
+          if(netWorkGroup[i].standard == netWorkGroup[j].standard){
+            resGroup.push(netWorkGroup[i])
+          }
+          if(j==netWorkGroup.length-1 && resGroup.length!=0){
+            resGroup.push(netWorkGroup[i])
+          }
+        }
+        netWorkGroup = netWorkGroup.filter(item=>{return item.standard != netWorkGroup[i].standard})
+        console.log(netWorkGroup);
       }
     },
     _splitNetWork(network) {
